86f3adc55137083f727307a802a9f7c0105e1f09
efex50 <aefex50@gmail.com>
2025-02-14 13:41:21 +0300

    güncelleme

--- src/nasm_efe/nasm_compiler.rs
+++ src/nasm_efe/nasm_compiler.rs
@@ -6,99 +6,97 @@ use crate::{funs::trim_zeroes, instruction::Instuction, ops::OpCodes};
 
 use super::PreCompile;
 
-
-
-#[derive(Debug,)]
-enum Objects{
-    Instruction{
-        i:Instuction,
-        len:usize
+#[derive(Debug)]
+enum Objects {
+    Instruction {
+        i: Instuction,
+        len: usize,
     },
-    Jump{
-        type_:OpCodes,
-        label:String
+    Jump {
+        /// type is like jmp, jne, je
+        type_: OpCodes,
+        label: String,
     },
-    AfterJump{
-        type_:OpCodes,
-        label:String,
+    AfterJump {
+        type_: OpCodes,
+        label: String,
         /// size of the jump instruction
-        jump:Vec<u8>
+        jump: Vec<u8>,
     },
     Label(String),
-    AfterLabel(String,usize)
+    AfterLabel(String, usize),
 }
 #[derive(Debug)]
-pub struct Data{
-    v :Vec<Objects>,
-    labels:HashMap<String,usize>
+pub struct Data {
+    v: Vec<Objects>,
+    labels: HashMap<String, usize>,
 }
-impl Data{
-    pub(crate)  fn from_pre_compile(a:Vec<PreCompile>) -> Self{
+impl Data {
+    pub(crate) fn from_pre_compile(a: Vec<PreCompile>) -> Self {
         let mut v: Vec<Objects> = Vec::new();
         let mut labels = HashMap::new();
-        for x in a{
+        for x in a {
             match x {
-                PreCompile::I(inst) => {
-                    match inst.opcode {
-                        OpCodes::Jmp |
-                        OpCodes::Je |
-                        OpCodes::Jne |
-                        OpCodes::Jgt |
-                        OpCodes::Jlt |
-                        OpCodes::Jle |
-                        OpCodes::Jge |
-                        OpCodes::Call |
-                        OpCodes::Jz |
-                        OpCodes::Jnz => {
-                            match inst.operandl{
-                                crate::nasm_efe::Operands::Label(l) => {
-                                    v.push(Objects::Jump { type_: inst.opcode, label: l });
-                                },
-                                _ => todo!(),
-                            };
-                        },
-                        _ => {
-                            let len = inst.get_len();
-                            v.push(Objects::Instruction { i: inst, len: len });
-                        },
+                PreCompile::I(inst) => match inst.opcode {
+                    OpCodes::Jmp
+                    | OpCodes::Je
+                    | OpCodes::Jne
+                    | OpCodes::Jgt
+                    | OpCodes::Jlt
+                    | OpCodes::Jle
+                    | OpCodes::Jge
+                    | OpCodes::Call
+                    | OpCodes::Jz
+                    | OpCodes::Jnz => {
+                        match inst.operandl {
+                            crate::nasm_efe::Operands::Label(l) => {
+                                v.push(Objects::Jump {
+                                    type_: inst.opcode,
+                                    label: l,
+                                });
+                            }
+                            _ => todo!(),
+                        };
+                    }
+                    _ => {
+                        let len = inst.get_len();
+                        v.push(Objects::Instruction { i: inst, len: len });
                     }
                 },
-                PreCompile::L(l) =>{
+                PreCompile::L(l) => {
                     labels.insert(l.clone(), 0);
                     v.push(Objects::Label(l))
-                },
+                }
             }
         }
-        
-        Self{v,labels}
+
+        Self { v, labels }
     }
-    pub fn r1(&mut self){
+    pub fn r1(&mut self) {
         let mut current_len = 0;
-        for x in &mut self.v{
+        for x in &mut self.v {
             match x {
-                Objects::Instruction { i:_, len } => {
+                Objects::Instruction { i: _, len } => {
                     current_len += *len;
-                },
-                Objects::Jump { type_:_, label:_ } => {
+                }
+                Objects::Jump { type_: _, label: _ } => {
                     current_len += 2;
-                    
-                },
+                }
                 Objects::Label(l) => {
                     self.labels.insert(l.to_string(), current_len);
                     *x = Objects::AfterLabel(l.clone(), current_len);
-
-                },
+                }
                 _ => todo!(),
             }
         }
     }
-    pub fn r2(&mut self){
+    pub fn r2(&mut self) {
         let mut current_len = 0;
-        for x in &mut self.v{
+        for x in &mut self.v {
             match x {
-                Objects::Instruction { i:_i, len } => {
+                Objects::Instruction { i: _i, len } => {
                     current_len += *len;
-                },
+                }
                 Objects::Jump { type_, label } => {
                     let labeld = self.labels.get(label).unwrap();
                     let mut staticval = Vec::new();
@@ -108,14 +106,22 @@ impl Data{
                     staticval.push(addr.len() as u8);
                     staticval.append(&mut addr);
                     current_len += staticval.len();
-                    *x = Objects::AfterJump { type_: type_.clone(), label: label.clone(), jump: staticval }
-                },
+                    *x = Objects::AfterJump {
+                        type_: type_.clone(),
+                        label: label.clone(),
+                        jump: staticval,
+                    }
+                }
                 Objects::AfterLabel(l, _addr) => {
                     let s = l.clone();
                     *x = Objects::AfterLabel(s.clone(), current_len);
                     self.labels.insert(s, current_len);
-                },
-                Objects::AfterJump { type_, label, jump: _jump } => {
+                }
+                Objects::AfterJump {
+                    type_,
+                    label,
+                    jump: _jump,
+                } => {
                     let labeld = self.labels.get(label).unwrap();
                     let mut staticval = Vec::new();
                     staticval.push(type_.to_u8().unwrap());
@@ -124,40 +130,43 @@ impl Data{
                     staticval.push(addr.len() as u8);
                     staticval.append(&mut addr);
                     current_len += staticval.len();
-                    *x = Objects::AfterJump { type_: type_.clone(), label: label.clone(), jump: staticval }
-
-                },
+                    *x = Objects::AfterJump {
+                        type_: type_.clone(),
+                        label: label.clone(),
+                        jump: staticval,
+                    }
+                }
                 _ => {
-                    println!("panic {:?}",x);
+                    println!("panic {:?}", x);
                     panic!()
-
-                },
+                }
             }
         }
     }
 
-
-
-    pub fn to_porgram(&self) -> Vec<u8>{
+    pub fn to_porgram(&self) -> Vec<u8> {
         let mut p = Vec::new();
 
-        for x in &self.v{
+        for x in &self.v {
             match x {
-                Objects::Instruction { i, len:_ } => {
+                Objects::Instruction { i, len: _ } => {
                     let mut px = i.get_program();
                     p.append(&mut px);
-                },
-                Objects::Jump { type_:_, label:_ } => todo!(),
-                Objects::AfterJump { type_:_, label:_, jump } => {
+                }
+                Objects::Jump { type_: _, label: _ } => todo!(),
+                Objects::AfterJump {
+                    type_: _,
+                    label: _,
+                    jump,
+                } => {
                     let mut px = jump.clone();
                     p.append(&mut px);
-                },
+                }
                 Objects::Label(_) => (),
                 Objects::AfterLabel(_, _) => (),
             }
         }
 
         p
-    } 
-
+    }
 }
