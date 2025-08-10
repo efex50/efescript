8e020e3362e19cd94ca333e068cafcc22f3c35ae
efex50 <aefex50@gmail.com>
2025-08-10 17:02:33 +0300

    rewrite

--- src/debugger/mod.rs
+++
@@ -1,5 +0,0 @@
-use crate::prelude::ProgramRuntime;
-
-pub struct Debugger{
-    runtime:ProgramRuntime
-}
--- src/prelude.rs
+++
@@ -1,3 +0,0 @@
-#[allow(dead_code,unused)]
-pub use crate::runtime::program::ProgramRuntime;
-pub use crate::nasm_efe::{parse_from_file,parse_str};
